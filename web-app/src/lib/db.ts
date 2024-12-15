656b70a31f77ebda531c4d700252311d0ba1bd8b
AleSenior <aesenior.20@est.ucab.edu.ve>
2024-12-14 20:31:06 -0400

    Readaptada la conexión a BD para usar variables ambientales

--- web-app/src/lib/db.ts
+++ web-app/src/lib/db.ts
@@ -1,11 +1,12 @@
 import postgres from 'postgres';
+import { PG_DATABASE, PG_HOST, PG_PORT, PG_USER, PG_PASSWORD } from '$env/static/private';
 
 const sql = postgres({
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '12345678',
-    database: 'fea_db'
+    host: PG_HOST,
+    port: parseInt(PG_PORT),
+    username: PG_USER,
+    password: PG_PASSWORD,
+    database: PG_DATABASE
 });
 
 export default sql;
